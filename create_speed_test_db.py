2cf6288520cd31e48bafccdadaf77d2f70853c35
annnvv <13822260+annnvv@users.noreply.github.com>
2022-10-09 14:20:48 -0400

    separated class definition from db table creation

--- create_speed_test_db.py
+++ create_speed_test_db.py
@@ -3,17 +3,8 @@
 ##JSONB info: https://amercader.net/blog/beware-of-json-fields-in-sqlalchemy/
 
 import yaml
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import (
-    create_engine,
-    Column,
-    Identity,
-    Integer,
-    Float,
-    DateTime,
-)
-from sqlalchemy.ext.mutable import MutableDict
-from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy import create_engine
+from define_speed_test_class import SpeedTest
 
 ## Create engine
 with open("database/config.yaml", "r") as f:
@@ -24,29 +15,6 @@ engine = create_engine(
     echo=True,
 )
 
-## Define table
-Base = declarative_base()
-
-
-class SpeedTest(Base):
-    __tablename__ = "speed_test"
-
-    speed_test_id = Column(
-        Integer,
-        Identity(start=1, increment=1, always=True, cycle=True),
-        primary_key=True,
-        unique=True,
-        nullable=False,
-    )
-    timestamp = Column(DateTime)
-    download_speed_Mb = Column(Float)
-    upload_speed_Mb = Column(Float)
-    bytes_sent = Column(Integer)
-    bytes_received = Column(Integer)
-    ping = Column(Float)
-    server_info = Column(MutableDict.as_mutable(JSONB))
-
-
-# Base.metadata.drop_all(engine)
+SpeedTest.metadata.drop_all(engine)
 ## Create table
-Base.metadata.create_all(engine)
+SpeedTest.metadata.create_all(engine)
---
+++ define_speed_test_class.py
@@ -0,0 +1,32 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (
+    Column,
+    Identity,
+    Integer,
+    Float,
+    DateTime,
+)
+from sqlalchemy.ext.mutable import MutableDict
+from sqlalchemy.dialects.postgresql import JSONB
+
+## Define table
+Base = declarative_base()
+
+
+class SpeedTest(Base):
+    __tablename__ = "speed_test"
+
+    speed_test_id = Column(
+        Integer,
+        Identity(start=1, increment=1, always=True, cycle=True),
+        primary_key=True,
+        unique=True,
+        nullable=False,
+    )
+    timestamp = Column(DateTime)
+    download_speed_Mb = Column(Float)
+    upload_speed_Mb = Column(Float)
+    bytes_sent = Column(Integer)
+    bytes_received = Column(Integer)
+    ping = Column(Float)
+    server = Column(MutableDict.as_mutable(JSONB))
