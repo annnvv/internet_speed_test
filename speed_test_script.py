369a89f79420fc2ffb2a5d0688db79cbe48c64b9
annnvv <13822260+annnvv@users.noreply.github.com>
2022-10-09 14:53:47 -0400

    converted script to functions

--- speed_test_script.py
+++ speed_test_script.py
@@ -2,12 +2,28 @@
 # speedtest-cli is compatible with Python up to 3.7
 
 import yaml
+from typing import Dict
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+# from prefect import Task, Flow
 import speedtest
+
 from define_speed_test_class import SpeedTest
 
 
+## EXTRACT function
+def get_speed_test_data() -> Dict:
+    speed_test = speedtest.Speedtest(secure=True)
+    speed_test.get_best_server()
+
+    speed_test.download()
+    speed_test.upload()
+
+    dict = speed_test.results.dict()
+    return dict
+
+## TRANSFORM function
 def bytes_to_mb(bytes):
     """Convert bytes to megabytes"""
 
@@ -15,43 +31,38 @@ def bytes_to_mb(bytes):
     MB = KB * 1024  # One Mb is 1024 Kb
     return bytes / MB
 
+def transform_speed_test_data(dict: Dict) -> Dict:
+    dict["download_speed_Mb"] = bytes_to_mb(dict["download"])
+    dict["upload_speed_Mb"] = bytes_to_mb(dict["upload"])
 
-## EXTRACT
-speed_test = speedtest.Speedtest(secure=True)
-speed_test.get_best_server()
+    del (
+        dict["client"],
+        dict["share"],
+        dict["download"],
+        dict["upload"],
+    )  ## remove from dictionary, do not need this info
 
-speed_test.download()
-speed_test.upload()
-
-results = speed_test.results.dict()
-
-## TRANSFORM
-results["download_speed_Mb"] = bytes_to_mb(results["download"])
-results["upload_speed_Mb"] = bytes_to_mb(results["upload"])
-
-del (
-    results["client"],
-    results["share"],
-    results["download"],
-    results["upload"],
-)  ## remove from dictionary, do not need this info
+    return dict
 
 
 ## LOAD
-with open("database/config.yaml", "r") as f:
-    config = yaml.safe_load(f)
+def etl_pipeline() -> None:
+    results = get_speed_test_data()
+    results_transformed = transform_speed_test_data(results)
 
-engine = create_engine(
-    f"postgresql+psycopg2://{config['db']['user_name']}:{config['db']['password']}@{config['db']['host_name']}:{config['db']['port']}/{config['db']['db_name']}",
-    echo=True,
-)
+    with open("database/config.yaml", "r") as f:
+        config = yaml.safe_load(f)
 
-Session = sessionmaker(bind=engine)
-s = Session()
+    engine = create_engine(
+        f"postgresql+psycopg2://{config['db']['user_name']}:{config['db']['password']}@{config['db']['host_name']}:{config['db']['port']}/{config['db']['db_name']}",
+        echo=True,
+    )
 
-data = SpeedTest(**results)
+    Session = sessionmaker(bind=engine)
+    s = Session()
 
-s.add(data)
+    data = SpeedTest(**results_transformed)
 
-s.commit()
-s.close()
+    s.add(data)
+    s.commit()
+    s.close()
