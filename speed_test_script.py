4167227daaf6d8640921035df8adc61bf63cb607
annnvv <13822260+annnvv@users.noreply.github.com>
2022-10-09 14:22:29 -0400

    restructured the script into an ETL pattern and added the database load part

--- speed_test_script.py
+++ speed_test_script.py
@@ -1,15 +1,22 @@
 # Based on: https://www.educative.io/answers/how-to-detect-internet-speed-using-python
 # speedtest-cli is compatible with Python up to 3.7
 
+import yaml
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 import speedtest
+from define_speed_test_class import SpeedTest
 
 
 def bytes_to_mb(bytes):
-    KB = 1024  # One Kilobyte is 1024 bytes
-    MB = KB * 1024  # One MB is 1024 KB
+    """Convert bytes to megabytes"""
+
+    KB = 1024  # One Kb is 1024 bytes
+    MB = KB * 1024  # One Mb is 1024 Kb
     return bytes / MB
 
 
+## EXTRACT
 speed_test = speedtest.Speedtest(secure=True)
 speed_test.get_best_server()
 
@@ -18,6 +25,33 @@ speed_test.upload()
 
 results = speed_test.results.dict()
 
-print(results["timestamp"])
-print("Your Download speed is", round(bytes_to_mb(results["download"]), 2), " Mb.")
-print("Your Upload speed is", round(bytes_to_mb(results["upload"]), 2), " Mb.")
+## TRANSFORM
+results["download_speed_Mb"] = bytes_to_mb(results["download"])
+results["upload_speed_Mb"] = bytes_to_mb(results["upload"])
+
+del (
+    results["client"],
+    results["share"],
+    results["download"],
+    results["upload"],
+)  ## remove from dictionary, do not need this info
+
+
+## LOAD
+with open("database/config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+
+engine = create_engine(
+    f"postgresql+psycopg2://{config['db']['user_name']}:{config['db']['password']}@{config['db']['host_name']}:{config['db']['port']}/{config['db']['db_name']}",
+    echo=True,
+)
+
+Session = sessionmaker(bind=engine)
+s = Session()
+
+data = SpeedTest(**results)
+
+s.add(data)
+
+s.commit()
+s.close()
