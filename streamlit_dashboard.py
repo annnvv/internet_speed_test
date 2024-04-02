38fa0f57dc46dbe0d198ccfb39e8c4a3442e311e
annnvv <13822260+annnvv@users.noreply.github.com>
2024-04-01 22:33:23 -0400

    added bottom two bar charts

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -20,6 +20,8 @@ timestamp = date_range('20240101 00:00','20240130 23:59',periods= 720)
 download_speed_Mb = np.random.normal(0, 300, size = 720)
 upload_speed_Mb = np.random.normal(0, 20, size = 720)
 df = DataFrame(dict(timestamp = timestamp, download_speed_Mb = download_speed_Mb, upload_speed_Mb = upload_speed_Mb)).reset_index(drop = True)
+df['date'] = df['timestamp'].dt.date
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 
 url = 'http://127.0.0.1:8000'
@@ -29,31 +31,50 @@ st.write(f"Average Internet Download Speed: {avg_speed['average_download_speed']
 
 middle_container = st.container()
 midcol1, midcol2 = st.columns(2)
-midcol1.header("Download - Timeseries")
 
-plt.figure(figsize=(10, 5))
+midcol1.header("Download - Timeseries")
 plt.plot(df['timestamp'], df['download_speed_Mb'], marker='o', linestyle='-')
 plt.title('Download Speed over Time')
 plt.xlabel('Timestamp')
+plt.xticks(rotation=45)
 plt.ylabel('Download Speed (Mbps)')
+plt.axhline(y=300, color='r', linestyle='--')
 plt.grid(True)
 plt.show()
-
 midcol1.pyplot() ##need argument here
 
-midcol2.header("Upload - Timeseries")
 
-plt.figure(figsize=(10, 5))
+midcol2.header("Upload - Timeseries")
 plt.plot(df['timestamp'], df['upload_speed_Mb'], marker='o', linestyle='-')
 plt.title('Upload Speed over Time')
 plt.xlabel('Timestamp')
+plt.xticks(rotation=45)
 plt.ylabel('Upload Speed (Mbps)')
 plt.grid(True)
 plt.show()
-
 midcol2.pyplot() ##need argument here
 
 bottom_container = st.container()
+avg_speeds_per_day = df.groupby('date')[['download_speed_Mb', 'upload_speed_Mb']].mean().reset_index()
 botcol1, botcol2 = st.columns(2)
+
 botcol1.header("Download - Average by day")
+plt.bar(avg_speeds_per_day['date'], avg_speeds_per_day['download_speed_Mb'])
+plt.title('Average Download Speed per Day')
+plt.xlabel('Date')
+plt.ylabel('Average Download Speed (Mbps)')
+plt.xticks(rotation=45)
+plt.grid(axis='y')
+plt.axhline(y=300, color='r', linestyle='--')
+plt.show()
+botcol1.pyplot()
+
 botcol2.header("Upload - Average by day")
+plt.bar(avg_speeds_per_day['date'], avg_speeds_per_day['upload_speed_Mb'])
+plt.title('Average Upload Speed per Day')
+plt.xlabel('Date')
+plt.ylabel('Average Upload Speed (Mbps)')
+plt.xticks(rotation=45)
+plt.grid(axis='y')
+plt.show()
+botcol2.pyplot()
