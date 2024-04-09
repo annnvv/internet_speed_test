82c8c2be602802059060345ef5cc452a3a7e18ea
annnvv <13822260+annnvv@users.noreply.github.com>
2024-04-09 01:02:38 -0400

    removed the fake data and connected the dashboard to the API endpoint

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -1,6 +1,7 @@
 import datetime
 import streamlit  as st
 import requests
+from pandas import DataFrame, to_datetime
 import matplotlib.pyplot as plt
 
 st.set_page_config(layout="wide")
@@ -10,23 +11,20 @@ st.title("Internet Speed Test Dashboard")
 top_container = st.container()
 topcol1, topcol2 = st.columns(2)
 current_year = datetime.date.today().year                         
-year = topcol1.selectbox('Year', range(2024, current_year+1))
+year = topcol1.selectbox('Year', range(2023, current_year+1)) 
 month = topcol2.selectbox('Month', range(1, 13))
 
-##this is dummy data to test the dashboard (actuall data will come from the API calls)
-from pandas import date_range, DataFrame
-import numpy as np
-timestamp = date_range('20240101 00:00','20240130 23:59',periods= 720)
-download_speed_Mb = np.random.normal(0, 300, size = 720)
-upload_speed_Mb = np.random.normal(0, 20, size = 720)
-df = DataFrame(dict(timestamp = timestamp, download_speed_Mb = download_speed_Mb, upload_speed_Mb = upload_speed_Mb)).reset_index(drop = True)
-df['date'] = df['timestamp'].dt.date
-
+url = 'http://127.0.0.1:8000'
 
 #TODO: use the year and month input to limit this query (will also need to update the endpoint to accept those two parameters)
-url = 'http://127.0.0.1:8000'
-avg_speed = requests.get(url = f'{url}/get_avg_download_speed').json()
-st.write(f"Average Internet Download Speed: {avg_speed['average_download_speed']:.2f} Mbps")
+# avg_speed = requests.get(url = f'{url}/get_avg_download_speed').json()
+# st.write(f"Average Internet Download Speed: {avg_speed['average_download_speed']:.2f} Mbps")
+
+monthly_params = {"year": year, "month": month}
+month_data = requests.get(url = f'{url}/get_monthly_data/', params = monthly_params).json()
+month_data_df = DataFrame(month_data)
+month_data_df.timestamp = to_datetime(month_data_df.timestamp)
+month_data_df['date'] = month_data_df['timestamp'].dt.date
 
 
 middle_container = st.container()
@@ -34,7 +32,7 @@ midcol1, midcol2 = st.columns(2)
 
 midcol1.header("Download - Timeseries")
 fig, ax = plt.subplots()
-ax.plot(df['timestamp'], df['download_speed_Mb'], marker='o', linestyle='-')
+ax.plot(month_data_df['timestamp'], month_data_df['download_speed_Mb'], marker='o', linestyle='-')
 ax.set_title('Download Speed over Time')
 ax.set_xlabel('Timestamp')
 plt.xticks(rotation=45)
@@ -46,7 +44,7 @@ midcol1.pyplot(fig)
 
 midcol2.header("Upload - Timeseries")
 fig2, ax2 = plt.subplots()
-ax2.plot(df['timestamp'], df['upload_speed_Mb'], marker='o', linestyle='-')
+ax2.plot(month_data_df['timestamp'], month_data_df['upload_speed_Mb'], marker='o', linestyle='-')
 ax2.set_title('Upload Speed over Time')
 ax2.set_xlabel('Timestamp')
 plt.xticks(rotation=45)
@@ -57,7 +55,7 @@ midcol2.pyplot(fig2)
 
 
 bottom_container = st.container()
-avg_speeds_per_day = df.groupby('date')[['download_speed_Mb', 'upload_speed_Mb']].mean().reset_index()
+avg_speeds_per_day = month_data_df.groupby('date')[['download_speed_Mb', 'upload_speed_Mb']].mean().reset_index()
 botcol1, botcol2 = st.columns(2)
 
 botcol1.header("Download - Average by day")
